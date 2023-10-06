5cca19fef9168b9cde2c7b6e0a4244d94a48e8c1
Ricky <rzh4321@gmail.com>
2023-10-06 15:03:28 -0400

    enable CORS al origins

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/users', (req, res) => {
+router.get('/api/users', async (req, res) => {
   const a = {hi: 'hi'};
-  res.json(a);
+  const test = await fetch('https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images/picture-element-wide.png')
+  res.send(test);
 })
 
 module.exports = router;
